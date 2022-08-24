4481c3cb15a5eebf54f9104828dcb8ee8b4377e1
rezafaisal98 <rezha.faizal567@gmail.com>
2022-08-25 00:28:27 +0800

    add column category on books

--- models/Book.js
+++ models/Book.js
@@ -9,6 +9,10 @@ const Book = mongoose.Schema({
         type: String,
         required: true
     },
+    category: {
+        type: String,
+        required: true
+    },
     price: {
         type: Number,
         required: true
